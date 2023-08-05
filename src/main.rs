d99ed7a3c9e7af67efbcd1080bdfa47ad9d24089
P. Todd Decker <ptdecker@mac.com>
2023-08-04 22:38:59 -0500

    Chapter 3.3.2

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
+use axum::extract::Path;
 use axum::{routing::get, Router};
 
 #[tokio::main]
-async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+async fn main() -> Result<(), std::io::Error> {
+    let app = Router::new()
+        .route("/", get(hello))
+        .route("/:name", get(hello_name));
 
-    // run it with hyper on localhost:3000
-    axum::Server::bind(&"0.0.0.0:8000".parse().unwrap())
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+    axum::Server::bind(
+        &"0.0.0.0:8000"
+            .parse()
+            .expect("unable to parse binding address"),
+    )
+    .serve(app.into_make_service())
+    .await
+    .expect("unable to bind service");
+
+    Ok(())
+}
+
+async fn hello() -> &'static str {
+    "Hello, World!\n"
+}
+
+async fn hello_name(Path(name): Path<String>) -> String {
+    format!("Hello {}\n", name)
 }
